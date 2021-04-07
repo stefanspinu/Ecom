8da09a6125ea4947900ef0d43977fee4da6c9012
Stefan <stefanspinu3@gmail.com>
2021-04-07 21:48:59 +0300

    added important things

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +10,7 @@ PROJECT_ROOT = os.path.dirname(__file__)
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v5qqc)oed+arin1gh^$ug&c%g%m6mo5tqixqpu)m46%z+z@u9^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
